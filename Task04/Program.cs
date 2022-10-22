2814b3816d351a4cabe8188a2501b1f5ffedb31a
Sergey <cpartak.89@mail.ru>
2022-10-22 19:48:29 +0300

    Add Task 10

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -6,21 +6,31 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+// Console.WriteLine("Введите три числа:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int max = number1;
+
+// if (number2 > max)
+// {
+//     max = number2;
+
+//     if (number3 > max)
+//     {
+//         max = number3;
+//         Console.WriteLine($"max = {number3}");
+//     }
+//     else Console.WriteLine($"max = {number2}");
+// }
+// else Console.WriteLine($"max = {number1}");
+
+
 Console.WriteLine("Введите три числа:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
-
-if (number2 > max)
-{
-    max = number2;
-
-    if (number3 > max)
-    {
-        max = number3;
-        Console.WriteLine($"max = {number3}");
-    }
-    else Console.WriteLine($"max = {number2}");
-}
-else Console.WriteLine($"max = {number1}");
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+Console.WriteLine($"max = {max}");
---
+++ Task10/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 10: Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа. 
+// 3. Выполнить с помощью числовых операций (целочисленное деление,
+// остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Write("Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number > 99 && number < 1000)
+// {
+//     int div = number / 10;
+//     int secondDigit = div % 10;
+//     Console.WriteLine($"Вторая цифра числа {number} -> {secondDigit}");
+// }
+// else Console.WriteLine("Введите трёхзначное число!");
+
+
+
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindSecondDigit(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        int div = num / 10;
+        return div % 10;
+    }
+    return -1;
+}
+
+int res = FindSecondDigit(number);
+Console.WriteLine($"Вторая цифра числа {number} -> {res}");
+
