711ea392de1e070a0437474d43409f38888d8352
Sergey <cpartak.89@mail.ru>
2022-10-23 22:15:16 +0300

    Add task 10-15

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -15,14 +15,40 @@ if (n > 0)
     Console.WriteLine($"Чётные числа от 1 до {n}:");
     while (count <= n)
     {
-        if (count % 2 == 0)
-        {
+        if(count % 2 == 0)
+        {    
             Console.WriteLine($"{count}");
         }
         count++;
     }
 }
-else Console.WriteLine("Введите другое число!");
+else Console.Write("Введено некорректное число!");
+
+
+
+
+
+
+
+
+
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// if (n > 0)
+// {
+//     Console.WriteLine($"Чётные числа от 1 до {n}:");
+//     while (count <= n)
+//     {
+//         if (count % 2 == 0)
+//         {
+//             Console.WriteLine($"{count}");
+//         }
+//         count++;
+//     }
+// }
+// else Console.WriteLine("Введите другое число!");
 
 
 
---
+++ Task13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая 
+// 1. выводит третью цифру заданного числа или 
+// 2. сообщает, что третьей цифры нет. 
+// 3. Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"третья цифра числа {number} -> ");
+
+if (number >= 1000)
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+        if (number < 1000)
+        {
+            Console.Write($"{number % 10}");
+        }
+    }
+}
+else if (number > 99 && number < 1000)
+{
+int thirdDigit = number % 10;
+Console.Write($"{thirdDigit}");
+}
+else Console.Write("нет");
