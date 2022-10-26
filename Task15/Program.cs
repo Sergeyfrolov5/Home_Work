53be2e0b432b7bff38a80c68d97017484bbc77bb
Sergey <cpartak.89@mail.ru>
2022-10-26 20:32:41 +0300

    Delete Tasks

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -18,3 +18,26 @@ if (digit > 0 && digit < 8)
     else Console.Write($"{digit} -> да");
 }
 else Console.Write("Введено некорректное число!");
+
+
+
+// 17. Напишите программу, которая 
+// 1. принимает на вход координаты точки (X и Y), 
+// 2. причём X ≠ 0 и Y ≠ 0 и 
+// 3. выдаёт номер четверти плоскости, в которой находится эта
+// точка.
+
+Console.Write("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int Quarter(int xc, int yc)
+{
+if (xc > 0 && yc > 0) return 1;
+if (xc < 0 && yc > 0) return 2;
+if (xc < 0 && yc < 0) return 3;
+if (xc > 0 && yc < 0) return 4;
+return 0;
+}
+int quarter = Quarter(x, y);
